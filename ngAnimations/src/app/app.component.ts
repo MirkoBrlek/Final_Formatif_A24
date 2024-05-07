369b017a2356d630f92753ef7dadd487e484df90
Mathieu Briau <mathieu.briau@gmail.com>
2024-05-07 11:02:08 -0400

    Solution question animations

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -5,11 +5,49 @@ import { bounce, shakeX, tada } from 'ng-animate';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('shake', [transition(':increment', useAnimation(shakeX, {
+      params: { timing: 0.6 }
+    }))]),
+    trigger('bounce', [transition(':increment', useAnimation(bounce, {
+      params: { timing: 0.8 }
+    }))]),
+    trigger('tada', [transition(':increment', useAnimation(tada, {
+      params: { timing: 0.9 }
+    }))])
+  ]
 })
 export class AppComponent {
   title = 'ngAnimations';
 
+  rotate= false;
+
+  first = 0;
+  second = 0;
+  third = 0;
+
   constructor() {
+  }
+
+  rotateCube() {
+    if(this.rotate == false){
+      this.rotate = true;
+      setTimeout(() => {this.rotate = false;},750);
+    }
+  }
+
+  playNgAnimations(forever:boolean) {
+    this.first++;
+    setTimeout(() => {
+      this.second++;
+      setTimeout(() => {
+        this.third++;
+        if(forever)
+        setTimeout(() => {
+          this.playNgAnimations(true);
+        },900);
+      },700);
+    },600);
   }
 }
